935e079143ff7fb25108528a4a1c711e0534584e
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-05-10 13:18:15 -0400

    changes made

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,22 +14,22 @@ var db = require("../models");
 module.exports = function(app) {
 
   // GET route for getting all of the todos
-  app.get("/", function(req, res) {
+  app.get("/api/bands", function(req, res) {
 
   });
 
   // POST route for saving a new todo. You can create a todo using the data on req.body
-  app.post("/", function(req, res) {
+  app.post("/api/bands", function(req, res) {
 
   });
 
   // DELETE route for deleting todos. You can access the todo's id in req.params.id
-  app.delete("/", function(req, res) {
+  app.delete("/api/bands", function(req, res) {
 
   });
 
   // PUT route for updating todos. The updated todo will be available in req.body
-  app.put("/", function(req, res) {
+  app.put("/api/bands", function(req, res) {
 
   });
 };
