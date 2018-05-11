4c6fcaa4d91597546762ed8c50c4c8650351596e
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-05-11 12:03:21 -0400

    chnages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,32 +1,4 @@
-<<<<<<< HEAD
-var path = require("path");
 
-// Routes
-// =============================================================
-module.exports = function(app) {
-
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-
-  // cms route loads cms.html
-  app.get("/createProfile", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/createProfile.html"));
-  });
-
-  // blog route loads blog.html
-  app.get("/bandList", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/bandList.html"));
-  });
-
-  // authors route loads author-manager.html
-  app.get("/bandPage", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/bandPage.html"));
-  });
-=======
 // *********************************************************************************
 // html-routes.js - this file offers a set of routes for sending users to the various html pages
 // *********************************************************************************
@@ -57,6 +29,6 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/bandList.html"));
     });
 
->>>>>>> 83754a280213e962aa6419c25011a76c5314c975
+
 
 };
